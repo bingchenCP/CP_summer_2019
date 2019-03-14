046ff3faa87d5f07b8b371a2cf88eb649cc6fd3e
bc87355 <captbcbc@gmail.com>
2019-03-14 13:19:12 +0100

    First program Hello

--- src/Hello.java
+++ src/Hello.java
@@ -2,21 +2,23 @@ public class Hello {
     public static void main(String[] inputargs) {
         System.out.println("Hello!!!");
 
-        int a = 5;
-        int = c;
+        Integer a = 5;
+        Integer b, c;
         b = a;
         c = b + 4;
-        c = new Integer(value:5);
+        c = new Integer(5);
 
         if (c == a) System.out.println("c==a");
         if (c.equals(a)) System.out.println("c equals a");
 
         System.out.println("b=" + b);
         System.out.println("c=" + c);
+
         String s1 = "abc";
         String s2 = null;
 
         String s3 = s1.toUpperCase();
+
         System.out.println(s3.charAt(1));
         s1 = "Hello John";
 
@@ -24,6 +26,7 @@ public class Hello {
         System.out.println(j);
 
         System.out.println(s1.substring(3,6));
+
 
     }
 }
--- src/Numbers.java
+++ src/Numbers.java
@@ -1,14 +1,12 @@
 public class Numbers {
 
     public static void main(String[] args) {
-        float fl = 455.3;
-        double d1 = fl;
+        float f1 = 455.3f;
+        double d1 = f1;
         //float f2 = d1;
-
         int a = 6;
         long b = a;
         int c = Long.valueOf(b).intValue();
-
         Long bb = Long.valueOf(a);
         int cc = bb.intValue();
 
@@ -18,18 +16,16 @@ public class Numbers {
         i = 8;
         a = i++;
         System.out.println(a);
-
         i = 8;
         a = ++i;
         System.out.println(a);
-
+        // PI to the power of 2
         double res = Math.pow(Math.PI, 2);
         System.out.println(res);
-
+        System.out.println(i);
+        // the rest of division by 7
         a = i % 7;
         System.out.println(a);
-
-
 
     }
 }
