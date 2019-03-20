7a1c5ff3596f3ba4d59719cca8d7388a38cb1e23
bingchenCP <captbcbc@gmail.com>
2019-03-20 15:39:19 +0100

    Update

---
+++ src/Homework1.java
@@ -0,0 +1,8 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        /*
+        Please write a program that checks if a given email address (in a String variable) is valid.
+        Please do not use regular expressions.
+        */
+    }
+}
